5eb127b14d7e6342a5bad6737797107c1462cfba
Aravind Siddhanthi <aravind0331@gmail.com>
2025-10-24 17:21:23 -0500

    added heartstatus model to know which stage app represting (like empty,filling,filled and success), also did code changes in view to bring the heart and Next button

---
+++ lib/model/heart_start.dart
@@ -0,0 +1,4 @@
+//here I am defining the mentioned 4 stages of the entire Appplication i.e Empty,Filling,Filled,Success
+// viewmodel can uses this states to decide what type of UI,buttons to display.
+
+enum HeartStatus { empty, filling, filled, success }
